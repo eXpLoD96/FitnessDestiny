7db8bbf83609ff6e3b48c499dcdb0459cd746984
Mihael <levski1914forevrr@dir.bg>
2017-12-20 14:20:42 +0200

    Some more tests added

--- FitnessDestiny.Tests/Services/Blog/ArticleServiceTests.cs
+++ FitnessDestiny.Tests/Services/Blog/ArticleServiceTests.cs
@@ -2,6 +2,7 @@
 using FitnessDestiny.Data;
 using FitnessDestiny.Data.Models;
 using FitnessDestiny.Services.Blog.Implementations;
+using FitnessDestiny.Services.Blog.Models;
 using FitnessDestiny.Web.Infrastructure.Mapper;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
@@ -19,9 +20,31 @@ namespace FitnessDestiny.Tests.Services.Blog
         
 
         [Fact]
+        public async Task FindByIdAsyncShouldReturnCorrectArticle()
+        {
+            // Arrange
+            var db = this.GetDatabase();
+
+            var firstArticle = new Article { Id = 14, Title = "First article", Content = new String('p', 255) };
+            var secondArticle = new Article { Id = 25, Title = "Second article", Content = new String('a', 255) };
+            db.AddRange(firstArticle, secondArticle);
+            await db.SaveChangesAsync();
+
+            var articleService = new ArticleService(db);
+
+            // Act
+            var result = await articleService.ByIdAsync(secondArticle.Id);
+
+            // Assert 
+            result
+                .Id
+                .Should()
+                .Be(25);
+        }
+
+        [Fact]
         public async Task FindAsyncShouldReturnCorrectResultWithFilterAndOrder()
         {
-            
             // Arrange
             var db = this.GetDatabase();
             var firstArticle = new Article { Id = 1, Title = "First article", Content = new String('p', 255)};
@@ -46,7 +69,53 @@ namespace FitnessDestiny.Tests.Services.Blog
                 .HaveCount(2);
         }
 
-        private FitnessDestinyDbContext GetDatabase()
+        [Fact]
+        public async Task EditShouldReturnCorrectServiceModel()
+        {
+            // Arrange
+            var db = this.GetDatabase();
+            var firstArticle = new Article { Id = 1, Title = "First article", Content = new String('p', 255) };
+            db.Add(firstArticle);
+
+            await db.SaveChangesAsync();
+            var articleService = new ArticleService(db);
+
+            // Act
+            var result = await articleService.EditByIdAsync(firstArticle.Id);
+
+            // Assert
+            result
+                .Should()
+                .BeOfType<ArticleEditServiceModel>();
+        }
+
+        [Fact]
+        public async Task AddCommentShouldIncreaseCommentCount()
+        {
+            // Arrange
+            var db = this.GetDatabase();
+            var user = new User { Id = "admin", Email = "admin@admin.bg" };
+            var firstArticle = new Article { Id = 1, Title = "First article", Content = new String('p', 255) };
+            var articleComment = new ArticleComment { ArticleId = 1, Content = "Asd", Author = user, AuthorId = "admin" };
+
+            await db.Articles.AddAsync(firstArticle);
+            await db.Comments.AddAsync(articleComment);
+
+            await db.SaveChangesAsync();
+            var articleService = new ArticleService(db);
+
+            // Act
+            var result = articleService.AddCommentAsync(firstArticle.Id, articleComment.Content, user.Id);
+
+            // Assert
+            firstArticle
+                .Comments
+                .Count
+                .Should()
+                .Be(2);
+
+        }
+            private FitnessDestinyDbContext GetDatabase()
         {
             var dbOptions = new DbContextOptionsBuilder<FitnessDestinyDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
